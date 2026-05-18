2e819414e1f7b39a9a3a480d7337a345aee82268
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2026-05-18 14:43:50 +0600

    add: server

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import { database } from "./config/database";
+import { env } from "./config/env";
+import { app } from "./app";
+
+const bootstrap = async (): Promise<void> => {
+  await database.connect();
+
+  app.listen(env.port, () => {
+    console.log(`Server is running on http://localhost:${env.port}`);
+  });
+};
+
+void bootstrap();
