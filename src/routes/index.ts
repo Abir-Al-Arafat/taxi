11018b09f139c903a452fe0eee8d48819a924728
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2026-05-18 14:44:13 +0600

    add: routes

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+// import { accountRouter } from "../modules/account/account.route";
+// import { transactionRouter } from "../modules/transaction/transaction.route";
+
+const router = Router();
+
+// router.use("/accounts", accountRouter);
+// router.use("/transactions", transactionRouter);
+
+export { router as apiRouter };
