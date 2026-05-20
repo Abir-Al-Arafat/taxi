6867f3b78e5efe1dd0475fd1418176e7cb85c416
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2026-05-20 15:01:20 +0600

    update: error middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import multer from "multer";
+
 import { AppError } from "../core/errors/AppError";
 import { ResponseBuilder } from "../core/utils/apiResponse";
 
@@ -12,6 +14,30 @@ export const errorMiddleware = (
     res.status(err.statusCode).json(ResponseBuilder.failure(err.message));
     return;
   }
+  // Multer errors
+  if (err instanceof multer.MulterError) {
+    let message = err.message;
 
+    switch (err.code) {
+      case "LIMIT_FILE_SIZE":
+        message = "File size exceeds limit";
+        break;
+
+      case "LIMIT_UNEXPECTED_FILE":
+        message = "Unexpected file field";
+        break;
+
+      default:
+        message = err.message;
+    }
+
+    res.status(400).json(ResponseBuilder.failure(message));
+
+    return;
+  }
+  console.error("Unexpected error", {
+    error: err instanceof Error ? err.message : String(err),
+    stack: err instanceof Error ? err.stack : undefined,
+  });
   res.status(500).json(ResponseBuilder.failure("Internal server error"));
 };
