681c492fd9e2cde96726f82f255c2e3e9124a7fc
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2026-05-20 15:01:07 +0600

    add: env for sending mail

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,6 +7,8 @@ class Env {
   public readonly nodeEnv: string;
   public readonly port: number;
   public readonly databaseUrl: string;
+  public readonly emailHostUser: string;
+  public readonly emailHostPass: string;
 
   constructor() {
     this.nodeEnv = process.env.NODE_ENV || "development";
@@ -19,7 +21,23 @@ class Env {
       );
     }
 
+    if (!process.env.EMAIL_HOST_USER) {
+      throw new AppError(
+        "EMAIL_HOST_USER is missing in environment variables",
+        500,
+      );
+    }
+
+    if (!process.env.EMAIL_HOST_PASS) {
+      throw new AppError(
+        "EMAIL_HOST_PASS is missing in environment variables",
+        500,
+      );
+    }
+
     this.databaseUrl = process.env.DATABASE_URL;
+    this.emailHostUser = process.env.EMAIL_HOST_USER;
+    this.emailHostPass = process.env.EMAIL_HOST_PASS;
   }
 }
 
