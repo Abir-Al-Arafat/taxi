398f0ee1dcd752f59e184826bc1f0353b1cfacf8
Abir-Al-Arafat <antorabir@gmail.com>
2026-06-06 17:38:33 +0600

    update: api response

--- src/core/utils/apiResponse.ts
+++ src/core/utils/apiResponse.ts
@@ -1,28 +1,38 @@
+import type { HttpStatus } from "../../constants/statusCodes";
+
 export interface ApiResponse<T> {
   success: boolean;
+  status: HttpStatus;
   message: string;
   data?: T;
 }
 
 export class ResponseBuilder {
-  static success<T>(message: string, data?: T): ApiResponse<T> {
+  static success<T>(
+    message: string,
+    data: T | undefined,
+    status: HttpStatus,
+  ): ApiResponse<T> {
     if (typeof data === "undefined") {
       return {
         success: true,
+        status,
         message,
       };
     }
 
     return {
       success: true,
+      status,
       message,
       data,
     };
   }
 
-  static failure(message: string): ApiResponse<undefined> {
+  static failure(message: string, status: HttpStatus): ApiResponse<undefined> {
     return {
       success: false,
+      status,
       message,
     };
   }
