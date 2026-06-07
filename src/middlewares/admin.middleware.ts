ea5273902dbf1f145426c4ee423dd39c02e1b388
Abir-Al-Arafat <antorabir@gmail.com>
2026-06-07 17:26:20 +0600

    add: middleware for admin role

---
+++ src/middlewares/admin.middleware.ts
@@ -0,0 +1,22 @@
+// src/middlewares/admin.middleware.ts
+import type { Response, NextFunction } from "express";
+import type { AuthenticatedRequest } from "./auth.middleware";
+import { AppError } from "../core/errors/AppError";
+import HTTP_STATUS from "../constants/statusCodes";
+
+export const requireAdminRole = (
+  req: AuthenticatedRequest,
+  _res: Response,
+  next: NextFunction,
+): void => {
+  if (req.user?.role !== "admin") {
+    next(
+      new AppError(
+        "Access denied. Administrative authentication privileges required.",
+        HTTP_STATUS.FORBIDDEN,
+      ),
+    );
+    return;
+  }
+  next();
+};
