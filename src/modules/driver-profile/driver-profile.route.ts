8e71f21aaf299ecd8059701ae0cce21dc6257731
Abir-Al-Arafat <antorabir@gmail.com>
2026-06-07 14:15:43 +0600

    cleanup: "unnecessary code removed"

--- src/modules/driver-profile/driver-profile.route.ts
+++ src/modules/driver-profile/driver-profile.route.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import path from "path";
 import { authenticate } from "../../middlewares/auth.middleware";
 import { DriverProfileController } from "./driver-profile.controller";
 import {
@@ -17,51 +16,11 @@ import {
   driverProfileUploadsDirectory,
   ensureDirectoryExists,
 } from "../../shared/utilities/file.util";
-import {
-  createMultiFieldUploadMiddleware,
-  type FieldConfig,
-} from "../../middlewares/upload.middleware";
-import { IMAGE_MIME_TYPES } from "../../constants/upload.constants";
 
 const router = Router();
 ensureDirectoryExists(driverProfileUploadsDirectory);
 
 const driverProfileController = new DriverProfileController();
-
-// Custom filename function that sanitizes filenames
-const customFilename = (
-  _req: Express.Request,
-  file: Express.Multer.File,
-  callback: (error: Error | null, filename: string) => void,
-): void => {
-  const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-  const extension = path.extname(file.originalname);
-  const safeBaseName = path
-    .basename(file.originalname, extension)
-    .replace(/[^a-zA-Z0-9_-]/g, "_");
-
-  callback(null, `${safeBaseName}-${uniqueSuffix}${extension}`);
-};
-
-// Configure multi-field upload middleware
-const driverProfileUploadFields = createMultiFieldUploadMiddleware(
-  [
-    { name: "nidOrPassport", maxCount: 1, allowedMimeTypes: IMAGE_MIME_TYPES },
-    { name: "profileImage", maxCount: 1, allowedMimeTypes: IMAGE_MIME_TYPES },
-    {
-      name: "drivingLicenseImages",
-      maxCount: 10,
-      allowedMimeTypes: IMAGE_MIME_TYPES,
-    },
-    {
-      name: "vehicleRegistrationDocumentImages",
-      maxCount: 10,
-      allowedMimeTypes: IMAGE_MIME_TYPES,
-    },
-  ],
-  driverProfileUploadsDirectory,
-  customFilename,
-);
 
 router.get(
   "/status",
